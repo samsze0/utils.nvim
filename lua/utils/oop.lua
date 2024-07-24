44587119d73e57acee8613ac615a336bb7308dec
Sam Sze <mingsum.sam@gmail.com>
2024-07-24 08:00:00 +0800

    oop utils

--- lua/utils/oop.lua
+++ lua/utils/oop.lua
@@ -15,10 +15,27 @@ end
 
 -- Check if an object is an instance of a class
 --
+-- If the `is_parent` option is set to true, it will only check if the object is a parent of the class, rather
+-- than checking if it is a descendant.
+--
 ---@param o any
 ---@param class any
-M.is_instance = function(o, class)
-  return type(o) == "table" and o.__is_class and getmetatable(o) == class
+---@param opts? { is_parent: boolean }
+M.is_instance = function(o, class, opts)
+  opts = opts or {}
+
+  if type(o) ~= "table" or not o.__is_class then return false end
+
+  if opts.is_parent then return getmetatable(o) == class end
+
+  while o do
+    if o == class then return true end
+
+    o = getmetatable(o)
+    if o == o then break end -- Prevent infinite loop
+  end
+
+  return false
 end
 
 return M
